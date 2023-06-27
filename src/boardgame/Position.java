7a6f8545d9c8b9f1a7fbc3fa922e4c86cbff67f0
Luiza Klafke <luizak2306@gmail.com>
2023-06-27 11:42:34 -0300

    Class position created

---
+++ src/boardgame/Position.java
@@ -0,0 +1,34 @@
+package boardgame;
+
+public class Position {
+	
+	private Integer row;
+	private Integer column;
+	
+	public Position() {
+	}
+	
+	public Position(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+
+	public Integer getRow() {
+		return row;
+	}
+
+	public Integer getColumn() {
+		return column;
+	}
+
+	public void setValues(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+
+}
